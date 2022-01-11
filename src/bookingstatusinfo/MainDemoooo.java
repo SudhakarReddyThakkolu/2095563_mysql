e48e48f13c7ae5944e1ccf87e2d44e2b75c28948
SudhakarReddyThakkolu <96788172+SudhakarReddyThakkolu@users.noreply.github.com>
2022-01-12 00:19:36 +0530

    Add files via upload

---
+++ src/bookingstatusinfo/MainDemoooo.java
@@ -0,0 +1,17 @@
+package bookingstatusinfo;
+import java.util.List;
+
+
+
+public interface Customers {
+
+
+
+public int addCustomers(Customers customers);
+public List<Customers> getALLCustomers();
+public Customers getCustomersByRegisterNo(int registerno);
+public int updateCustomers(Customers customers);
+public int deleteCustomers(int registerno);
+
+}
+
---
+++ src/com/cognizant/shapes/Java.java
@@ -0,0 +1,10 @@
+package booking_info;
+
+public class MainDemo {
+
+	public static void main(String[] args) {
+	int[] [] mynum = {{1,2,3,4},{5,6,7,8}};
+	int x = mynum [1][2];
+	System.out.println(x);
+	}
+}
